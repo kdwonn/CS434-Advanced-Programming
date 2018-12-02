5aca348885789dc00b9d47f889dab29db91c3c58
kdwon <kdwon@postech.ac.kr>
2018-12-03 05:48:27 +0900

    project-server-broadcast

--- project/chat_server/src/main/scala/ChatServer.scala
+++ project/chat_server/src/main/scala/ChatServer.scala
@@ -1,24 +1,43 @@
 import java.io.{BufferedReader, InputStream, InputStreamReader, PrintStream}
 import java.net.{ServerSocket, Socket}
-
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.Future
 
 
 object ChatServer extends App{
-  case class User(name: String, socket: Socket, inStream: InputStream, outStream: PrintStream)
+  case class User(name: String, socket: Socket, textInStream: InputStream, textOutStream: PrintStream)
   val users = new TrieMap[String, User]()
   Future{launchServer}
 
-  def launchServer = {
+  def launchServer: Unit = {
     val ss = new ServerSocket(1026)
     while(true) {
       val socketAccept = ss.accept
       newUser(socketAccept)
     }
   }
-  def newUser(socketAccept: Socket) = {
-    val textInStream = new BufferedReader(new InputStreamReader(socketAccept.getInputStream))
+  def newUser(socketAccept: Socket): Unit = {
+    val textInStream = socketAccept.getInputStream
     val textoutStream = new PrintStream(socketAccept.getOutputStream)
+    val name = Future {
+      readOrWaitStream(textInStream)
+    }
+    name foreach { name =>
+      users += name -> User(name, socketAccept, socketAccept.getInputStream, textoutStream)
+    }
+  }
+  def readOrNotStream(textInStream: InputStream): Option[String] = {
+    val br = new BufferedReader(new InputStreamReader(textInStream))
+    if(br.ready()) Some(br.readLine()) else None
+  }
+  def readOrWaitStream(textInStream: InputStream): String = {
+    val br = new BufferedReader(new InputStreamReader(textInStream))
+    while(!br.ready()) Thread.sleep(10)
+    br.readLine()
+  }
+  def broadcast(user: User): Unit ={
+    readOrNotStream(user.textInStream) foreach { i =>
+      users foreach {(n, u) => u.textOutStream.println(u.name + " : " + i)}
+    }
   }
 }
