3e1736eec7c3a4ea5575c160ab0f13b561eed44b
kdwon <kdwon@postech.ac.kr>
2018-12-03 04:50:10 +0900

    project-server-newUser skeleton

--- project/chat_server/src/main/scala/ChatServer.scala
+++ project/chat_server/src/main/scala/ChatServer.scala
@@ -1,15 +1,23 @@
 import java.io.{BufferedReader, InputStream, InputStreamReader, PrintStream}
 import java.net.{ServerSocket, Socket}
+
 import scala.collection.concurrent.TrieMap
+import scala.concurrent.Future
 
 
 object ChatServer extends App{
   case class User(name: String, socket: Socket, inStream: InputStream, outStream: PrintStream)
   val users = new TrieMap[String, User]()
+  Future{launchServer}
 
   def launchServer = {
     val ss = new ServerSocket(1026)
-    val socketAccept = ss.accept
+    while(true) {
+      val socketAccept = ss.accept
+      newUser(socketAccept)
+    }
+  }
+  def newUser(socketAccept: Socket) = {
     val textInStream = new BufferedReader(new InputStreamReader(socketAccept.getInputStream))
     val textoutStream = new PrintStream(socketAccept.getOutputStream)
   }
