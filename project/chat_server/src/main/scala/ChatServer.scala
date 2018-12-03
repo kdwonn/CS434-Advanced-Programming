3a14eab57a02f793b6076d2767b771eb4b0a0564
kdwon <kdwon@postech.ac.kr>
2018-12-03 22:19:24 +0900

    project-server-status message added

--- project/chat_server/src/main/scala/ChatServer.scala
+++ project/chat_server/src/main/scala/ChatServer.scala
@@ -2,7 +2,7 @@ import java.io.{BufferedReader, InputStream, InputStreamReader, PrintStream}
 import java.net.{ServerSocket, Socket}
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.Future
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object ChatServer extends App{
   case class User(name: String, socket: Socket, textInStream: InputStream, textOutStream: PrintStream)
@@ -10,10 +10,12 @@ object ChatServer extends App{
   Future{launchServer}
   while(true){
     users foreach(p => broadcast(p._2))
+    Thread.sleep(100)
   }
 
   def launchServer: Unit = {
     val ss = new ServerSocket(1026)
+    println("[SERVER] started")
     while(true) {
       val socketAccept = ss.accept
       newUser(socketAccept)
@@ -23,24 +25,19 @@ object ChatServer extends App{
     val textInStream = socketAccept.getInputStream
     val textoutStream = new PrintStream(socketAccept.getOutputStream)
     val name = Future {
-      readOrWaitStream(textInStream)
+      IoCommon.readOrWaitStream(textInStream)
     }
-    name foreach { name =>
+    name foreach { name => {
+      println("[CLIENT] new user : " + name)
       users += name -> User(name, socketAccept, socketAccept.getInputStream, textoutStream)
     }
-  }
-  def readOrNotStream(textInStream: InputStream): Option[String] = {
-    val br = new BufferedReader(new InputStreamReader(textInStream))
-    if(br.ready()) Some(br.readLine()) else None
-  }
-  def readOrWaitStream(textInStream: InputStream): String = {
-    val br = new BufferedReader(new InputStreamReader(textInStream))
-    while(!br.ready()) Thread.sleep(10)
-    br.readLine()
+    }
   }
   def broadcast(user: User): Unit ={
-    readOrNotStream(user.textInStream) foreach { i =>
+    IoCommon.readOrNotStream(user.textInStream) foreach { i => {
+      println("[SERVER] Broadcasting new message : " + i)
       users foreach (p => p._2.textOutStream.println(user.name + " : " + i))
+    }
     }
   }
 }
