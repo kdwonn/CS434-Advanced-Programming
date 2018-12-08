0d3240f001f6799ad3f7d6a934f113f3ec12d98b
kdwon <kdwon@postech.ac.kr>
2018-12-08 11:48:29 +0900

    project-user-redesign user class

---
+++ project/chat_server/src/main/scala/User.scala
@@ -0,0 +1,26 @@
+import java.io.{ObjectInputStream, ObjectOutputStream}
+import java.net.Socket
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class User(val name: String, val socket: Socket, val inStream: ObjectInputStream, val outStream: ObjectOutputStream){
+  def writePacket(packet: Packet): Unit = {
+    try {
+      outStream.writeObject(packet)
+    }catch {
+      case _ => {Thread.sleep(10); writePacket(packet)}
+    }
+  }
+  def readPacket: Packet = {
+    try {
+      inStream.readObject().asInstanceOf[Packet]
+    }catch {
+      case e: ClassCastException => {println(e); Thread.sleep(10); readPacket}
+    }
+  }
+  def checkIncomingPacket(server: ChatServer.type): Future[Unit] = Future{
+    while(true){
+      println("[SERVER] [" + name + "] reading incoming packet")
+    }
+  }
+}
