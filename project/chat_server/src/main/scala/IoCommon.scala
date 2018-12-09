ee4e3e7d5b5853a4442420e36ff710a59ada7c07
kdwon <kdwon@postech.ac.kr>
2018-12-10 00:32:31 +0900

    proj-common-add write to stream

--- project/chat_server/src/main/scala/IoCommon.scala
+++ project/chat_server/src/main/scala/IoCommon.scala
@@ -5,7 +5,14 @@ object IoCommon {
     try{
       ois.readObject().asInstanceOf[Packet]
     }catch{
-      case e: ClassCastException => {println(e); Thread.sleep(10); readStream(ois)}
+      case e: ClassCastException => println(e); Thread.sleep(10); readStream(ois)
+    }
+  }
+  def writeStream(oos: ObjectOutputStream, packet: Packet): Unit = {
+    try {
+      oos.writeObject(packet)
+    }catch {
+      case e: IOException => println(e); Thread.sleep(10); writeStream(oos, packet)
     }
   }
 }
