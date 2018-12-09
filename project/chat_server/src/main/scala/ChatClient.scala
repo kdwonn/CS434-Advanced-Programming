b634107b36a52301a9d75d2b8dd74079473fd11f
kdwon <kdwon@postech.ac.kr>
2018-12-10 00:31:52 +0900

    proj-client-restruct code

--- project/chat_server/src/main/scala/ChatClient.scala
+++ project/chat_server/src/main/scala/ChatClient.scala
@@ -8,27 +8,38 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object ChatClient extends App {
   val port = 1026
   val socket = new Socket("localhost", 1026)
-  val inputStream = socket.getInputStream
-  val outputStream = socket.getOutputStream
+  val is = socket.getInputStream
+  val os = socket.getOutputStream
 
-  lazy val ois = new ObjectInputStream(inputStream)
-  lazy val oos = new ObjectOutputStream(outputStream)
+  lazy val inStream = new ObjectInputStream(is)
+  lazy val outStream = new ObjectOutputStream(os)
 
   print("Enter username : ")
   val name = readLine()
-  oos.writeObject(TextPacket(name, name))
+  sendPacket(TextPacket(name, name))
 
-  println("Welcome! Chatting Started")
-  Future{
-    while(true){
-      IoCommon.readStream(ois) match{
-        case TextPacket(m, f) => println(f + " : " + m)
-        case SoundPacket(m, f) => ???
-      }
-    }
-  }
+  //Future{new MicInputLine(this).listenThenSend()}
+
+  Future{decodeIncomingPacket}
+
   while(true){
     print(">>")
-    oos.writeObject(TextPacket(readLine(), name))
+    sendPacket(TextPacket(readLine(), name))
+  }
+
+  def sendPacket(packet: Packet): Unit = this.synchronized {
+    IoCommon.writeStream(outStream, packet)
+  }
+
+  def decodeIncomingPacket: Unit = {
+    println("Welcome! Chatting Started")
+    while(true){
+      IoCommon.readStream(inStream) match{
+        case TextPacket(m, f) => println(f + " : " + m)
+        case SoundPacket(m, f) => ???
+        case EmptyPacket(f) => ???
+      }
+      Thread.sleep(10)
+    }
   }
 }
