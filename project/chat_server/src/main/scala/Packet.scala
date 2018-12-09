fbe520cd7debf11ec2383910545b47477a41a979
kdwon <kdwon@postech.ac.kr>
2018-12-10 00:35:58 +0900

    proj-packet-refactor & add emptypacket

--- project/chat_server/src/main/scala/Packet.scala
+++ project/chat_server/src/main/scala/Packet.scala
@@ -1,24 +1,24 @@
 import javax.sound.sampled.AudioFormat
 
 trait Packet{
-  def message : Any
   def from : String
-  def getMessage : Any
 }
 
-class Sound(val bytes: List[Byte]) extends Serializable
+class Sound(val bytes: Array[Byte]) extends Serializable
 object Sound{
-  def apply(bytes: List[Byte]): Sound = new Sound(bytes)
+  def apply(bytes: Array[Byte]): Sound = new Sound(bytes)
   val defaultFormat = new AudioFormat(11025f, 8, 1, true, true);
   val defaultLength = 1200
 }
 
 case class TextPacket(message: String, from: String) extends Packet with Serializable{
-  override def getMessage: String = message
   override def toString: String = "[TEXT] : " + message
 }
 
 case class SoundPacket(message: Sound, from: String) extends Packet with Serializable{
-  override def getMessage: Sound = message
   override def toString: String = "[SOUND]"
+}
+
+case class EmptyPacket(from: String) extends Packet with Serializable{
+  override def toString: String = "[EMPTY]"
 }
